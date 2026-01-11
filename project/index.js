a3e5e69131dbbe56f6bab7ff491bf98b4952aca2
niharikagupta21022006-beep <you@example.com>
2026-01-11 16:15:02 +0530

    MYNTRA PROJECT FINISHED

--- project/index.js
+++ project/index.js
@@ -1,30 +1,40 @@
-let bagItems = [];
+let bagItems;
+onLoad();
+
+function onLoad(){
+    let bagItemStr = localStorage.getItem('bagItems');
+    bagItems = bagItemStr? JSON.parse(bagItemStr) : [];
+
+     displayItemsOnHomePage();
+     displayBagIcon();
+}
 
 
-displayItemsOnHomePage();
 
 function addToBag(itemId) {
     bagItems.push(itemId);
+    localStorage.setItem('bagItems',JSON.stringify(bagItems));
     displayBagIcon();
 }
 
 function displayBagIcon() {
-    let bagItemCountElement = document.querySelector('bag-item-count');
-    bagItemCountElement.innerHTML = bagItems.length;
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if(bagItems.length > 0){
+        console.log('I am here');
+        bagItemCountElement.style.visibility = 'visible';
+        bagItemCountElement.innerText = bagItems.length;
+    }
+    else{
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+    
 }
-
-
-
-
-
-
-
-
-
-
 
 function displayItemsOnHomePage() {
     let itemsContainerElement = document.querySelector('.items-container');
+    if(! itemsContainerElement) {
+        return;
+    }
 let innerHTML = '';
 items.forEach(item => {
     innerHTML += `
